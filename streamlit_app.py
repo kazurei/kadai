af851fe9c17a3259e1b503c78956774f44fdd397
kazurei <167180368+kazurei@users.noreply.github.com>
2024-07-18 07:52:08 +0000

    ああ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,7 @@
-import streamlt as st
+import streamlit as st
+import matplotlib.pyplot as plt
+x = int(st.text_input("国語"))
+y = int(st.text_input("数学"))
+z = int(st.text_input("理科"))
+v = int(st.text_input("社会"))
+b = int(st.text_input("英語"))
