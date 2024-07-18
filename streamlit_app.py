0839cf9b418c95565c4887300a86397720323418
kazurei <167180368+kazurei@users.noreply.github.com>
2024-07-18 08:23:35 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ z = st.number_input("理科")
 v = st.number_input("社会")
 b = st.number_input("英語")
 
-n = ["国語","数学","理科","社会","英語"]
+n = ["japanese","math","science","social study","english"]
 height = [x,y,z,v,b]
 fig, ax = plt.subplots()
 ax.bar(n, height, width=0.5)
