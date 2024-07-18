2481e35f46875de941127cb7612f233dd0ccbece
kazurei <167180368+kazurei@users.noreply.github.com>
2024-07-18 08:01:45 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import matplotlib.pyplot as plt
-x = int(st.text_input("国語"))
-y = int(st.text_input("数学"))
-z = int(st.text_input("理科"))
-v = int(st.text_input("社会"))
-b = int(st.text_input("英語"))
+x = st.number_input("国語")
+y = st.number_input("数学")
+z = st.number_input("理科")
+v = st.number_input("社会")
+b = st.number_input("英語")
 
 n = ["国語","数学","理科","社会","英語"]
 height = [x,y,z,v,b]
