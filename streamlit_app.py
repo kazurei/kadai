52c38379efb3f10e644c77e5e00d006174a59906
kazurei <167180368+kazurei@users.noreply.github.com>
2024-07-18 08:05:29 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import matplotlib.pyplot as plt
+import numpy as np
 x = st.number_input("国語")
 y = st.number_input("数学")
 z = st.number_input("理科")
@@ -9,14 +10,4 @@ b = st.number_input("英語")
 n = ["国語","数学","理科","社会","英語"]
 height = [x,y,z,v,b]
 plt.bar(n, height, width=0.5)  
-plt.show()
-
-
-
-
-
-
-x = ['A', 'B', 'C', 'D', 'E']
-height = [3, 7, 2, 4, 6]
-plt.bar(x, height, width=0.5)  # 幅0.5の縦棒グラフを作成
 plt.show()
