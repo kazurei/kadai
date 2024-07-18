8bddbfec5784fdf8626da28a6f994d23d31dd23f
kazurei <167180368+kazurei@users.noreply.github.com>
2024-07-18 08:37:03 +0000

    aa

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,16 @@ fig, ax = plt.subplots()
 ax.bar(n, height, width=0.5)
 st.pyplot(fig)
 
+label_list = ["japanese","math","science","social study","english"]
+acc_list = [x,y,z,v,b]
+acc_list += acc_list[:1]
+angle_list = [n / float(len(label_list)) * 2 * np.pi for n in range(len(label_list))]
+angle_list += angle_list[:1]
+fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
+plt.xticks(angle_list[:-1], label_list, color='grey', size=12)
+
+ax.plot(angle_list, acc_list, linewidth=1, linestyle='solid', label='Model A')
+ax.fill(angle_list, acc_list, 'blue', alpha=0.1)
+
+plt.legend(loc='upper right')
+plt.show()
